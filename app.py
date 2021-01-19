7b42b2e5f830caba6c11e2ac9235c5137a6ef687
Jacky <jacky85031085@gmail.com>
2021-01-19 15:32:44 +0900

    add wheather

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info("Request  body: " + body)
 
     # handle webhook body
     try:

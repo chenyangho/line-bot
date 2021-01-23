9234451ff6c517b6a1729a08f1af2bdb975d423d
Jacky <jacky85031085@gmail.com>
2021-01-23 18:55:28 +0900

    add flexmessage

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ def handle_message(event):
                             alt_text='hello',
                             contents=result[0]
                         )
-        line_bot_api.reply_message(event.reply_token, FlexSendMessage(flex_message)
+        line_bot_api.reply_message(event.reply_token, FlexSendMessage(flex_message))
 
     else:
         line_bot_api.reply_message(
@@ -102,7 +102,7 @@ def word_check(message):
 
 # creater
 def creater():    
-    
+
     json_text = {'type': 'bubble','hero': {'type': 'image','url': 'https://i.imgur.com/4hng01H.jpg','size': 'full'},'body': {'type': 'box','layout': 'vertical','contents': [{'type': 'text','text': '陳暘和','color': '#487e95','weight': 'bold','style': 'normal','decoration': 'none','position': 'relative','size': 'xl'},{'type': 'box','layout': 'vertical','contents': [{'type': 'text','text': 'ECCコンピューター専門学校　在学','size': 'sm'}]},{'type': 'box','layout': 'baseline','contents': [{'type': 'icon','url': 'https://www.flaticon.com/svg/vstatic/svg/941/941565.svg?token=exp=1611393042~hmac=b1c4644da87587d8bf41510c6d395c35','size': 'xl','offsetTop': 'lg'},{'type': 'text','text': 'jacky85031085@gmail.com','margin': 'md','offsetTop': 'sm'}],'paddingAll': 'none'}]}}
     return [json_text, "flex_message"]
     

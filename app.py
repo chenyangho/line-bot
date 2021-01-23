911e81121c305945023ec8db23712048bafc0ec5
Jacky <jacky85031085@gmail.com>
2021-01-23 18:32:39 +0900

    add flexmessage

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def word_check(message):
     elif "天気" in message:
         return weather_japan()
     elif "Show creater" in message:
-        return creater()
+        creater()
     else:
         return database_word(message)
 
@@ -98,7 +98,7 @@ def  creater():
     alt_text='hello',
     contents={'type': 'bubble','hero': {'type': 'image','url': 'https://i.imgur.com/4hng01H.jpg','size': 'full'},'body': {'type': 'box','layout': 'vertical','contents': [{'type': 'text','text': '陳暘和','color': '#487e95','weight': 'bold','style': 'normal','decoration': 'none','position': 'relative','size': 'xl'},{'type': 'box','layout': 'vertical','contents': [{'type': 'text','text': 'ECCコンピューター専門学校　在学','size': 'sm'}]},{'type': 'box','layout': 'baseline','contents': [{'type': 'icon','url': 'https://www.flaticon.com/svg/vstatic/svg/941/941565.svg?token=exp=1611393042~hmac=b1c4644da87587d8bf41510c6d395c35','size': 'xl','offsetTop': 'lg'},{'type': 'text','text': 'jacky85031085@gmail.com','margin': 'md','offsetTop': 'sm'}],'paddingAll': 'none'}]}}
 )
-    return flex_message
+    line_bot_api.reply_message(reply_token, FlexSendMessage(flex_message))
 
 
 # 翻譯功能
